e67f76f8f471237e2de21ff832b15297d3bbdf70
PerolaCristina1 <franca.perola@escola.pr.gov.br>
2024-09-27 09:47:21 -0300

    Add files via upload

---
+++ anime-main/ANIMES FLECDSCARD/perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Dragon Ball',
+    'Qual o nome da forma animal de Goku?',
+    'Oozaru'
+)
+
+criaCartao(
+    'Kimetsu no Yaiba',
+    'Qual a comida favorita de Kyōjurō rengoku?',
+    'Batata Doce'
+)
+
+criaCartao(
+    'One Piece',
+    'Quem ficou de proteger durante 2 anos o Navio de Luffy Sunny Go?',
+    'Kuma'
+)
+
+criaCartao(
+    'One Punch Man',
+    ' Qual foi o único inimigo que Saitama não conseguiu derrotar?',
+    'Um mosquito'
+)
+
+criaCartao(
+    'Naruto',
+    ' Quais animais são invocados pelos 3 Sannins Lendarios?',
+    'Sapo, Lesma e Cobra'
+)
